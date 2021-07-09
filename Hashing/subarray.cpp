3919e7bc11a7acd4c083cf1ade76c40efd77544b
Sachin <67999058+its-sachin@users.noreply.github.com>
2021-07-10 00:17:16 +0530

    corner case handled

--- Hashing/subarray.cpp
+++ Hashing/subarray.cpp
@@ -12,7 +12,7 @@ int solve(vector<int> &A, int B) {
     vector<int> v;
     int c = 0;
     for (int i = 0; i < A.size(); i++){
-        
+
         if(A[i]&1){
             v.push_back(i);
         }
@@ -21,11 +21,11 @@ int solve(vector<int> &A, int B) {
     if(v.size()==0){
         if(B!=0) return 0;
 
-        return A.size()+1;
+        return pow(2,A.size())-1;
     };
     if(v.size()==1){
         if(B>1) return 0;
-        return (1+v[0])*(A.size()-v[0]);
+        return (v[0])*(A.size()-v[0]);
     }
 
     int i=0;
@@ -34,17 +34,15 @@ int solve(vector<int> &A, int B) {
 
     c=0;
     while(j<v.size()){
-        int i1=0;
+        int i1=-1;
         if(i>0) i1= v[i-1];
-        int j1 = A.size()-1;
+        int j1 = A.size();
         if(j!=v.size()-1) j1=v[j+1];
-        c+= (1+v[i]-i1)*(1+j1-v[j]);
+        c+= (v[i]-i1)*(j1-v[j]);
+        i+=1;
+        j+=1;
     }
     return c;
-
-
-
-
 }
 
 int main(int argc, char const *argv[])
